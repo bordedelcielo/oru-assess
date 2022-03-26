c411fb76bcff1259a8ab20e2c2db24f0bd04328a
bordedelcielo <christopher.thrutchley@gmail.com>
2022-03-25 23:08:30 -0500

    current branch

---
+++ my-solutions/script.js
@@ -0,0 +1,42 @@
+// console.log(document.querySelector("a").clientHeight);
+
+// jQuery( window ).on( "load", function() {
+//     console.log( "document loaded" );
+//     console.log(document.querySelector("a"));
+//     console.log(document.querySelector("a").clientHeight);
+//     console.log(document.getElementById("a").clientHeight);
+//     console.log(document.getElementById("a").clientWidth);
+//     console.log(document.getElementById("virtualtour_iframe_0"));
+//     console.log(document.querySelector("iframe"));
+//     if( window.canRunAds === undefined ){
+//         // adblocker detected, show fallback
+//         // showFallbackImage();
+//         console.log('Cannot run ads')
+//       };
+// } );
+
+// $( document ).ready(function() {
+//     console.log( "document loaded" );
+//     console.log(document.querySelector("a").clientHeight);
+//     console.log(document.getElementById("a").clientHeight);
+//     console.log(document.querySelector("iframe"));
+//     console.log(document.querySelector("a"));
+//     console.log(document.getElementById("script"));
+// });
+
+// function iniFrame() {
+//     if ( window.location !== window.parent.location )
+//     {
+      
+//         // The page is in an iFrames
+//         console.log("The page is in an iFrame");
+//     } 
+//     else {
+          
+//         // The page is not in an iFrame
+//         console.log("The page is not in an iFrame");
+//     }
+// }
+  
+// // Calling iniFrame function
+// iniFrame();
